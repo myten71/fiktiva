700740c9ce8a779021bd3dd3244d2a9939cd9305
myten71 <simennysaether@live.no>
2023-07-05 23:52:13 +0100

    favicon

--- kontakt.php
+++ kontakt.php
@@ -1,27 +1,26 @@
 <?php
 if (isset($_POST['submit'])) {
-  // Get form inputs
+ 
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
   
-  // Set up email
-  $to = 'simen@simennysaether.no'; // Replace with your email address
+
+  $to = 'simen@simennysaether.no'; // 
   $subject = 'New Contact Form Submission';
   $headers = "From: $name <$email>" . "\r\n";
-  
-  // Compose the email body
+
   $email_body = "Name: $name\n\n";
   $email_body .= "Email: $email\n\n";
   $email_body .= "Message:\n$message";
   
-  // Send the email
+
   if (mail($to, $subject, $email_body, $headers)) {
-    // Email sent successfully
-    echo 'Thank you for your message. We will get back to you shortly.';
+    
+    echo 'Din henvendelse er sendt. Vi kommer tilbake til deg snarest.';
   } else {
-    // Failed to send email
-    echo 'Sorry, an error occurred while sending your message. Please try again later.';
+  
+    echo 'Beklager, din beskjed ble ikke sendt - prøv igjen senere';
   }
 }
 ?>
