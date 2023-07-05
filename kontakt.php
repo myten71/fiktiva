f689b819eb9977ee29cf90ac65e9e877709c10c8
myten71 <simennysaether@live.no>
2023-07-05 23:03:18 +0100

    Fikset logo i header + bildeendringer
    endret bilder manuelt i Adobe AI for farger, fikset bug med logo i header

--- kontakt.php
+++ kontakt.php
@@ -1,35 +1,27 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Retrieve form data
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
-
-  // Validate form data
-  $errors = array();
-  if (empty($name)) {
-    $errors[] = "Name is required";
-  }
-  if (empty($email)) {
-    $errors[] = "Email is required";
-  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = "Invalid email format";
-  }
-  if (empty($message)) {
-    $errors[] = "Message is required";
-  }
-
-  // If there are no errors, send the email
-  if (empty($errors)) {
-    $to = "simen@simennysaether.no"; // Replace with your email address
-    $subject = "Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
-
-    if (mail($to, $subject, $body)) {
-      $success_message = "Thank you for your message. We will get back to you soon!";
-    } else {
-      $error_message = "Oops! Something went wrong. Please try again later.";
-    }
+if (isset($_POST['submit'])) {
+  // Get form inputs
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  // Set up email
+  $to = 'simen@simennysaether.no'; // Replace with your email address
+  $subject = 'New Contact Form Submission';
+  $headers = "From: $name <$email>" . "\r\n";
+  
+  // Compose the email body
+  $email_body = "Name: $name\n\n";
+  $email_body .= "Email: $email\n\n";
+  $email_body .= "Message:\n$message";
+  
+  // Send the email
+  if (mail($to, $subject, $email_body, $headers)) {
+    // Email sent successfully
+    echo 'Thank you for your message. We will get back to you shortly.';
+  } else {
+    // Failed to send email
+    echo 'Sorry, an error occurred while sending your message. Please try again later.';
   }
 }
 ?>
